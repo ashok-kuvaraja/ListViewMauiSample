980b096d8d93e53a2a6e756aecce3ff581e281be
ashok-kuvaraja <ashok.kuvaraja@syncfusion.com>
2023-01-06 15:28:04 +0530

    Updated the sample.

---
+++ SimpleMauiApp/SimpleMauiApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace SimpleMauiApp
+{
+
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+
+	class ViewModel
+	{
+		List<String> names = new List<string>();
+
+		public List<String> Names
+		{
+			get
+			{
+				return names;
+			}
+		}
+
+		public ViewModel()
+		{
+			GenerateNames(100);
+		}
+
+		void GenerateNames(int count)
+		{
+			for (int i = 0; i < count; i++)
+				names.Add("Label " + i.ToString());
+		}
+	}
+}
+
